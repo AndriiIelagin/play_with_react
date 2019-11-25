9755c14c6b768071691797087c66be2df9d7e0cc
Andrew Elagin <andriiielagin@gmail.com>
2019-11-25 11:22:14 +0200

    Multiple useReducers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import ComplexReducerCounter from './components/ComplexReducerCounter';
+import MultipleReducerCounter from './components/MultipleReducerCounter';
 
 function App() {
   return (
     <div className="App">
-      <ComplexReducerCounter />
+      <MultipleReducerCounter />
     </div>
   );
 }
