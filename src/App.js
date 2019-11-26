9fa7ea83cda63c6bc57cdbf28a900ad9abfa438f
AndrewElagin <AndrewElagin@users.noreply.github.com>
2019-11-26 15:56:14 +0200

    make useDocumentTitle custom hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HookTimer from './components/HookTimer';
+import DocTitleOne from './components/DocTitleOne';
 
 function App() {
   return (
     <div className="App">
-      <HookTimer />
+      <DocTitleOne />
     </div>
   );
 }
